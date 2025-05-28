35a6fa1defd0b96126584acaaf1cfa24e1d5737a
OneEyeX <chedly199@gmail.com>
2025-05-28 14:52:32 +0100

    Global message works

--- src/main/java/org/example/pfabackend/controllers/ChatRestController.java
+++ src/main/java/org/example/pfabackend/controllers/ChatRestController.java
@@ -3,12 +3,14 @@ package org.example.pfabackend.controllers;
 import lombok.RequiredArgsConstructor;
 import org.example.pfabackend.entities.ChatMessageEntity;
 import org.example.pfabackend.repositories.ChatMessageRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/chat")
 @RequiredArgsConstructor
